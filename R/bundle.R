43ffa8405c8fe8756cd47cd8355e0411ab273ee0
Eric Bryant <EricEdwardBryant@gmail.com>
2018-12-24 13:58:58 -0500

    Automate bundle listing

--- R/bundle.R
+++ R/bundle.R
@@ -3,88 +3,20 @@
 #' This package bundle's my frequently used R packages to make installation
 #' easier on a new system.
 #'
+#' @importFrom desc desc
+#' @importFrom stringr str_trim
 #' @export
 
 bundle <- function() {
+  description <- desc::desc(package = "bundle.eeb")
+
   list(
-    cran = c(
-      "ape",
-      "assertthat",
-      "bookdown",
-      "broom",
-      "caTools",
-      "cgdsr",
-      "cli",
-      "crayon",
-      "data.table",
-      "dplyr",
-      "DBI",
-      "dbplyr",
-      "devtools",
-      "DT",
-      "feather",
-      "forcats",
-      "fuzzyjoin",
-      "ggplot2",
-      "ggrepel",
-      "googlesheets",
-      "haven",
-      "here",
-      "hms",
-      "httr",
-      "igraph",
-      "investr",
-      "jsonlite",
-      "knitr",
-      "lubridate",
-      "magrittr",
-      "MASS",
-      "miniUI",
-      "modelr",
-      "pbapply",
-      "purrr",
-      "R.utils",
-      "Rcpp",
-      "readr",
-      "readxl",
-      "RefManageR",
-      "remotes",
-      "reprex",
-      "rhandsontable",
-      "rlang",
-      "rmarkdown",
-      "roxygen2",
-      "rstudioapi",
-      "RSQLite",
-      "Rtsne",
-      "rvest",
-      "servr",
-      "shiny",
-      "stringr",
-      "stringi",
-      "testthat",
-      "tibble",
-      "tidyr",
-      "tidyverse",
-      "usethis",
-      "xml2",
-      "yaml"
-    ),
-    bioc = c(
-      "BSgenome",
-      "Biostrings",
-      "EBImage",
-      "msa",
-      "flowCore"
-    ),
-    github = c(
-      "EricEdwardBryant/easydb",
-      "EricEdwardBryant/rothfreezer",
-      "EricEdwardBryant/screenmill",
-      "EricEdwardBryant/rothSGA",
-      "EricEdwardBryant/YeastBarcodes",
-      "EricEdwardBryant/iSTOP",
-      "EricEdwardBryant/mutagenesis"
-    )
+    cran   = parse_deps(description$get("Imports")),
+    bioc   = parse_deps(description$get("Suggests")),
+    github = parse_deps(description$get("Remotes"))
   )
+}
+
+parse_deps <- function(deps) {
+  stringr::str_trim(strsplit(deps, "\\s*,\\s*", perl = TRUE)[[1]])
 }
